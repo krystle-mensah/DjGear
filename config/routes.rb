226c24053aa6d3626a5f5d5d42928b4c0d69d5d1
Kryswebdev <krystle.webdev@gmail.com>
2018-04-11 02:17:55 +0100

    content added to contact,about and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  
-  get 'contact/contact'
+    resources :items
+    
+    get 'contact/contact'
 
-  get 'about/about'
-
-  resources :items
+    get 'about/about'
   
-  
-  root 'home#index'
+    
+    root 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
