28ff3467887ed04025fa9775929a44bf1100805e
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-03 12:48:23 +0100

    products controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    resources :products
+    
     resources :items
     
     get 'contact/contact'
@@ -9,4 +11,4 @@ Rails.application.routes.draw do
     root 'home#index'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+end 
