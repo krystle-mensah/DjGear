99e41cde0ded08565b405f4a246fe9005fbe1e1c
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-03 14:54:24 +0100

    added a instance var to index product controller  + each loop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
     def index
-
+        @products = Product.all
     end 
+    
     def show 
 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
     
     resources :items
     
+    root 'home#index'
+    
     get 'contact/contact'
 
     get 'about/about'
   
     
-    root 'home#index'
+    
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end 
