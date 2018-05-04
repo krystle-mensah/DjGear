668823f0e68b78cce6b378e8971114567133619b
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-04 13:12:50 +0100

    new scss folder + showpage.scss

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+    
     def index
         @products = Product.all
     end 
     
     def show 
-
     end
 
     def new
@@ -12,10 +13,10 @@ class ProductsController < ApplicationController
 	end  
 
     def edit 
-
     end
+
     def create
-        @product = Product.new(product_params) 
+		@product = Product.new(product_params) 
 
 		respond_to do |format|
 			if @product.save
@@ -26,13 +27,27 @@ class ProductsController < ApplicationController
 				format.json { render json: @product.errors, status: :unprocessable_entity }
 			end
 		end
-    end
+	end
+
     def update
+		respond_to do |format|
+			if @product.update(product_params)
+				format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+				format.json { render :show, status: :ok, location: @product }
+			else
+				format.html { render :edit }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-    end
     def destroy
-
-    end
+		@product.destroy
+		respond_to do |format|
+		format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+		format.json { head :no_content }
+		end
+	end
 
     private
 		# Use callbacks to share common setup or constraints between actions.
