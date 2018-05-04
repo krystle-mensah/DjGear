230f9e4e0f8f68c6d4e81b19beb868e61610c2a4
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-04 10:02:17 +0100

    images added + migration colour change +

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,17 @@ class ProductsController < ApplicationController
 
     end
     def create
+        @product = Product.new(product_params) 
 
+		respond_to do |format|
+			if @product.save
+				format.html { redirect_to @product, notice: 'Product was successfully created.' }
+				format.json { render :show, status: :created, location: @product }
+			else
+				format.html { render :new }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
     end
     def update
 
@@ -25,6 +35,19 @@ class ProductsController < ApplicationController
     end
 
     private
+		# Use callbacks to share common setup or constraints between actions.
+		def set_product
+				@product = Product.find(params[:id])
+		end
+
+		# Never trust parameters from the scary internet, only allow the white list through.
+        # does the below code define the variable of the controller actions
+        
+        # This method acts as a whitelist filter for data that gets saved into the database—it's a security feature ensuring that users won’t be able to update fields that you don’t want them to unless allowed by your application
+        
+        def product_params
+		params.require(:product).permit(:name, :description, :image_url, :colour)
+		end   
     
 
 end
