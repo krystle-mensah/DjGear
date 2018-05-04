1e953cf20773bb97ea96f32c0ec2d146bc1c2252
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-04 17:22:51 +0100

    new orders controller created

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+# meaning it has access to all the methods inside your application_controller.rb file
+class OrdersController < ApplicationController 
+    def index
+    end
+  
+    def show
+    end
+  
+    def new
+    end
+  
+    def create
+    end
+  
+    def destroy
+    end
+
+end
+
