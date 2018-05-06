2219ad6ca87258a254306543914bae164faa71a6
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-06 22:06:20 +0100

    modifed order.index+order.show+order.controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 # meaning it has access to all the methods inside your application_controller.rb file
 class OrdersController < ApplicationController 
     def index
+        # all the orders made by users
+        @orders = Order.all
     end
   
     def show
+        @order = Order.find(params[:id])
     end
   
     def new
