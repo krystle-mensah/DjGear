1069073a0a0c669946d273dd9f0e96afd5457cd4
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-06 19:17:38 +0100

    carserol added to landing index.

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,6 +2,7 @@ class LandingPagesController < ApplicationController
     # This method will retrieve the first product in the Products table and set it to a variable called @featured_product 
     def index
         @featured_product = Product.first
+        @products = Product.all
     end
 
 end
