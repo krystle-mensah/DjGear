e0f2d9f44e87cd5497755a139a1f13c00f4a5940
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-11 10:26:38 +0100

    5.3 - new user_mailer.rb + modified app_mailer.rb.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# This is where you'll define the default email address that will show up in the “from” field of emails that get sent from your application. Go ahead and exchange form@example.com with the email address you want to use on your app.
+
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'krystle.webdev@gmail.com'
   layout 'mailer'
 end
