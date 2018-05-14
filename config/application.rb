f57ea4622a23ccde76a6918883938a5c76873e27
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-14 12:50:08 +0100

    4.7 - modified gemfile + app/layouts + app.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 module DjGear
   class Application < Rails::Application
