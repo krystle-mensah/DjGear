dccd50a10da4df29637044de7b5f29f6a7b4523c
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-20 21:31:43 +0100

    5.5 - started styling

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,11 @@
-class Ability
+# below code (defining user permissions)
+class Ability 
     include CanCan::Ability
 
+    # (takes in a user object as a parameter, so permissions can be modified based on any user attributes.
     def initialize(user)
         user ||= User.new # guest user (not logged in)
+        # action to the argument can. :manage matches any action on the User controller, meaning any user can manage (perform any action) with a User object. 
         can :manage, User, id: user.id
     end
-
 end
