7d182f7e548b0d40bf4e8479bbf1d1e24ada0041
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-05-21 16:02:16 +0100

    5.4 - before_action :authenticate_user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,8 @@
 # meaning it has access to all the methods inside your application_controller.rb file
 class OrdersController < ApplicationController 
+    # below code means unless you're signed in, you won't be able to access any of the order actions.
+    before_action :authenticate_user
+
     def index
         # all the orders made by users
         @orders = Order.all
