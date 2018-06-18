f24a96261ea7f27a2d23e85d13dfd25f06edcff7
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-18 14:55:26 +0100

    styling + code comments

--- db/schema.rb
+++ db/schema.rb
@@ -12,51 +12,51 @@
 
 ActiveRecord::Schema.define(version: 20180523091215) do
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
-    t.text "body"
-    t.integer "rating"
-    t.integer "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_comments_on_product_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+    create_table "comments", force: :cascade do |t|
+        t.integer "user_id"
+        t.text "body"
+        t.integer "rating"
+        t.integer "product_id"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["product_id"], name: "index_comments_on_product_id"
+        t.index ["user_id"], name: "index_comments_on_user_id"
+    end
 
-  create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "product_id"
-    t.float "total"
-    t.index ["product_id"], name: "index_orders_on_product_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
+    create_table "orders", force: :cascade do |t|
+        t.integer "user_id"
+        t.integer "product_id"
+        t.float "total"
+        t.index ["product_id"], name: "index_orders_on_product_id"
+        t.index ["user_id"], name: "index_orders_on_user_id"
+    end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "colour"
-  end
+    create_table "products", force: :cascade do |t|
+        t.string "name"
+        t.text "description"
+        t.string "image_url"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.string "colour"
+    end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+    create_table "users", force: :cascade do |t|
+        t.string "first_name"
+        t.string "last_name"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.string "email", default: "", null: false
+        t.string "encrypted_password", default: "", null: false
+        t.string "reset_password_token"
+        t.datetime "reset_password_sent_at"
+        t.datetime "remember_created_at"
+        t.integer "sign_in_count", default: 0, null: false
+        t.datetime "current_sign_in_at"
+        t.datetime "last_sign_in_at"
+        t.string "current_sign_in_ip"
+        t.string "last_sign_in_ip"
+        t.index ["email"], name: "index_users_on_email", unique: true
+        t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
 
 end
