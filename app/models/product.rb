1d27628ae7cc2f0b1794b6b037ad717d7187d8f5
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-19 15:28:47 +0100

    5.9 - validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,24 @@
 class Product < ApplicationRecord
     has_many :orders
     has_many :comments
-    
+# Let’s add a validation to the Product model that requires all new products to have a name provided.
+    validates :name, presence: true
+
     def self.search(search_term)
         like_string = Rails.env.production? ? "ilike" : "LIKE"
         Product.where("name #{like_string} ?", "%#{search_term}%")
     end
-    
+
     # rate the first commit in descending order
     def highest_rating_comment
         comments.rating_desc.first
     end
-    
+
     # rate first commit ascending order
     def lowest_rating_comment
         comments.rating_asc.first
     end
-    
+
     def average_rating
         comments.average(:rating).to_f
     end
@@ -24,4 +26,4 @@ class Product < ApplicationRecord
     def highest_rating_body
         comments.rating_desc.first.body
     end
-end    
+end
