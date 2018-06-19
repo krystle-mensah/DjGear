54edeefa87e150e4f44d1522ad5635794b09427b
Krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-19 15:26:02 +0100

    5.9 - validation for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,20 @@ class CommentsController < ApplicationController
         @comment.user = current_user
         @comment.save
         redirect_to product_path(@product)
-    end
-  
+
+
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to @product, notice: 'Review was created successfully.' }
+                format.json { render :show, status: :created, location: @product }
+            else
+                format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+                format.json { render json: @comment.errors, status: :unprocessable_entity }
+            end
+        end
+
+     end #end create
+
     def destroy
     end
 
