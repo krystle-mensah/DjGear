42b2dec7088aae3b604ebeda1277bfed19b34ea2
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-23 22:26:26 +0100

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,37 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
-	devise_for :users
-	resources :users
-	resources :orders, only: [:index, :show, :create, :destroy]
+    post '/rate' => 'rater#create', :as => 'rate'
+    	# devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: {registrations: "user_registrations"}
+    	devise_for :users
+        resources :users
+    	resources :orders, only: [:index, :show, :create, :destroy]
 
-    # resources :products
-    
-    # This is called a nested resource. this is a defintion 
-    resources :products do
-        resources :comments
-    end
+        # get '/user/sign_up'
 
-    resources :items
-    
-    get '/products', to: 'products#index'
-    
-    get 'home/index'
-    
-    # contact routes 
-    get 'contact/contact'    
-    post '/contact/thank_you'
-    
-    get 'about/about'
 
-    get 'landing_pages/index'
-    
-    
-    root 'landing_pages#index'  
+
+        # resources :products
+        # This is called a nested resource. this is a defintion 
+        resources :products do
+            resources :comments
+        end
+
+        resources :items
+        
+        get '/products', to: 'products#index'
+        
+        get 'home/index'
+        
+        # contact routes 
+        get 'contact/contact'    
+        post '/contact/thank_you'
+        get '/contact/thank_you' 
+        
+        get 'about/about'
+
+        get 'landing_pages/index'
+        
+        
+        root 'landing_pages#index'  
 end
 
 
