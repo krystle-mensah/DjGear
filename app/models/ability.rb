902a6c74cdba309d9b365ef00db352897f16a44b
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-23 10:38:30 +0100

    Restricting Access of Destroy Method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,11 @@ class Ability
     # (takes in a user object as a parameter, so permissions can be modified based on any user attributes.
     def initialize(user)
         user ||= User.new # guest user (not logged in)
-        # action to the argument can. :manage matches any action on the User controller, meaning any user can manage (perform any action) with a User object. 
-        can :manage, User, id: user.id
+        if user.admin?
+        	can:manage, :all
+        else
+	        # action to the argument can. :manage matches any action on the User controller, meaning any user can manage (perform any action) with a User object. 
+	        can :manage, User, id: user.id
+        end
     end
 end
