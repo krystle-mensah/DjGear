ef2153c2e854a5043be8f076191c1e9bb36fad67
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-06-29 17:41:39 +0100

    5.5 - migratation for price of products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180622122553) do
+ActiveRecord::Schema.define(version: 20180629163819) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180622122553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
