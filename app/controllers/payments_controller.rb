75426170d7b5e99584974039e8df78062677fe2a
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-07-12 10:04:02 +0100

    6.6 - making payments.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,32 +1,31 @@
 class PaymentsController < ApplicationController
     def create
     	token = params[:stripeToken]
-        @product = Product.find(params[:product_id])
+        # @product = Product.find(params[:product_id])
         @user = current_user
 
         begin
             charge = Stripe::Charge.create(
-                amount: (@product.price * 100).to_i,
+                amount: (@product.price*100).to_i,
                 currency: "gbp",
                 source: token,
                 description: params[:stripeEmail]
             )
         
         if charge.paid
-            Order.create(product_id: @product.id, user_id: @user.id,
-				product_id: @product.id,
-				user_id: @user.id,
-                total: @product.price.to_i)
-            
+            Order.create(
+                product_id: @product.id, 
+                user_id: @user.id, 
+                total: @product.price.to_i
+            )
             flash[:success] = "Your payment was processed successfully"
         end
 
-
         # The card has been declined
         rescue Stripe::CardError => e
-            
-        
-
+            body = e.json_body
+            err = body[:error]
+            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}" 
         end #begin
 
         redirect_to product_path(@product)
