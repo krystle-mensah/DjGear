d15a3450b859a3f09f4a5a6f9b5e8f80764eb0eb
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-07-17 11:44:41 +0100

    6.6 - payments

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key: ENV['STRIPE_SECRET_KEY']
-  }
+	Rails.configuration.stripe = {
+	    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+	    secret_key: ENV['STRIPE_SECRET_KEY']
+    }
 else
-  Rails.configuration.stripe = {
-    publishable_key: 'pk_test_J9dogvbCu4bkn7xOgK50F8qF',
-    secret_key: 'sk_test_UQYUFFuwYVXbzaNgJP4RdtP8'
-  }
+    Rails.configuration.stripe = {
+	    publishable_key: 'pk_test_J9dogvbCu4bkn7xOgK50F8qF',
+	    secret_key: 'sk_test_UQYUFFuwYVXbzaNgJP4RdtP8'
+    }
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
