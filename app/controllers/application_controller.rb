ee8aa8ad2ccbd077637d87dae64c606ad8ef36ce
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-07-29 21:02:58 +0100

    tidying code. up to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 	# Now when we try editing another user, we get redirected to the homepage and this flash error appears at the top of the page, appearing in the div tags we added when we installed Devise:
 	rescue_from CanCan::AccessDenied do |exception|
 		redirect_to main_app.root_url, alert: exception.message
