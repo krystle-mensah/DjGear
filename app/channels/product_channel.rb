a201533a7b60a3b7a9f1321cfad9a2be9f573ab9
krystle Mensah <krystlemensah@Krystles-MacBook-Pro.local>
2018-08-08 20:33:57 +0100

    6.14

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,10 +1,14 @@
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    # Now if users subscribe to the channel, they start “streaming” all the updates
-    stream_from "product_channel"
+    # stream_from "product_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+  
+  def listen(data)
+    stop_all_streams
+    stream_for data["product_id"]
   end
 end
